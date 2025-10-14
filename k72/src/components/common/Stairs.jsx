c5a7489c04bc268b5d925fe2ebc99e956082f909
Ankurkumar45 <ankurkumardwivedi779@gmail.com>
2025-10-14 18:29:55 +0530

    Initial commit

---
+++ k72/src/components/common/Stairs.jsx
@@ -0,0 +1,61 @@
+import React, { startTransition, useRef } from 'react'
+import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { useLocation } from 'react-router-dom'
+
+export default function Stairs(props) {
+
+    const currentPath = useLocation().pathname
+    // console.log(currentPath)
+    const stairParentRef = useRef(null)
+    const pageRef = useRef(null)
+    useGSAP(function () {
+        const tl = gsap.timeline()
+
+        tl.to(stairParentRef.current, {
+            display: 'block'
+        })
+        tl.from('.stair', {
+            height: 0,
+            stagger: {
+                amount: -0.25
+            }
+        })
+        tl.to('.stair', {
+            y: '100%',
+            stagger: {
+                amount: -0.25
+            }
+        })
+        tl.to(stairParentRef.current, {
+            display: 'none'
+        })
+        tl.to('.stair', {
+            y: '0%'
+        })
+
+        gsap.from(pageRef.current, {
+            opacity: 0,
+            delay: 1.3,
+            scale: 1.2
+        })
+    }, [currentPath])
+    // console.log(props.children)
+    console.log(pageRef.current)
+    return (
+        <>
+            <div ref={stairParentRef} className='h-screen w-full fixed z-20 top-0'>
+                <div className='h-full w-full flex'>
+                    <div className="stair h-full w-1/5 bg-black"></div>
+                    <div className="stair h-full w-1/5 bg-black"></div>
+                    <div className="stair h-full w-1/5 bg-black"></div>
+                    <div className="stair h-full w-1/5 bg-black"></div>
+                    <div className="stair h-full w-1/5 bg-black"></div>
+                </div>
+            </div>
+            <div ref={pageRef}>
+                {props.children}
+            </div>
+        </>
+    );
+}
